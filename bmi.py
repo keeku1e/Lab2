f3a1e9a3bef9d6df594d487120742b2bb8817f7e
Kuan Ee <kuanee.23@ichat.sp.edu.sg>
2024-04-30 09:53:19 +0800

    modified calculate_bmi

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(weight, height):
     print("BMI=", bmi)
     if bmi < 18.5:
         print("Under Weight")
+        return -1
     elif bmi <= 25.0: 
         print("Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 calculate_bmi(57, 1.73)
